a21b0adaf5bc0e8f300acd41be043ca1bd2c8568
Eric Mwangi <ericmwas01@gmail.com>
2025-03-31 10:47:45 +0300

    listing 2.2. Having bad thread in C++

---
+++ bad_thread_1.cpp
@@ -0,0 +1,26 @@
+#include <thread>
+void do_something(int &i){
+    i++; 
+}
+
+struct func {
+    func(int &i_): i(i_){}
+    void operator()(){
+        for(unsigned j=0; j < 1000000; j++){
+            do_something(i); 
+        }
+    }
+    
+
+    int &i; 
+}; 
+
+
+int main(){
+    int local_variable = 0;
+
+    func myfunc(local_variable); 
+
+    std::thread thread_1(myfunc); 
+    thread_1.detach(); 
+}
